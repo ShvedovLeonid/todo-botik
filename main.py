42c88e28c20d2af195a23829508c3b971a23f16d
movavi_school <movavi_school@macpc-5248.local>
2024-12-08 13:01:56 +0700

    Added new files

---
+++ main.py
@@ -0,0 +1,14 @@
+from telebot import TeleBot
+from constants import TOKEN
+from todo.controlles import Controller
+
+
+bot = TeleBot(TOKEN)
+controller = Controller(bot)
+
+
+controller.register_handlers()
+
+if __name__ == "__main__":
+    print("Bot is running...")
+    bot.polling()
